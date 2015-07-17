55589f3c6b9551ab81bca351e0e6c41834f851a3
Ray Yang <jkman357@gmail.com>
2015-07-17 10:36:58 +0800

    first init

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express().use(express.static(__dirname + '/'));
+
+app.set('express_port', (process.env.PORT || 5000));
+
+app.listen(app.get('express_port'), function() {
+  console.log('Node app is running on port', app.get('express_port'));
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + 'index.html');
+});
+
+
+//var http = require('http').Server(app);
+//var httpsocket = require('socket.io')(http);
+
+//var PeerServer = require('peer').PeerServer;
+//var	PEER_PORT = process.env.PORT || 9000;
+//var EXPRESS_PORT = process.env.PORT || 5000;
+/*
+var options = {
+	port: PEER_PORT,
+	path: '/',
+	key: 'peerjs'
+};
+*/
+//var server = new PeerServer(options);
+//var peers = [];
+
+
+//httpsocket.on('connection', function(socket){
+//   console.log('a user connected');
+//});
+/*
+http.listen(EXPRESS_PORT, function(){
+    console.log("Node server listening on port " + EXPRESS_PORT);
+});
+*/
+
+/*
+server.on('connection', function (id) {
+	var now = new Date();
+	peers.push(id);
+	console.log('[peer]','connecting peer id:', id);
+	httpsocket.emit('message', {'message': now.toLocaleString() + ' connecting peer id : ' + id});
+});
+
+server.on('disconnect', function (id) {
+	var now = new Date();
+	
+	if (peers.indexOf(id) !== -1) {
+		peers.splice(peers.indexOf(id), 1);
+	}
+	
+	console.log('[peer]', 'disconnecting peer id:', id);
+	httpsocket.emit('message', {'message':  now.toLocaleString() + ' disconnecting peer id : ' + id});
+});
+*/
